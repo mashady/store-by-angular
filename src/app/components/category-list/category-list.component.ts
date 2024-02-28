66ecf74eacafacc0bd9ed2a6602d9f8879669560
mashady <muhammedmashady@gmail.com>
2024-02-28 20:13:48 +0200

    00

--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -10,32 +10,38 @@ export class CategoryListComponent {
   CategoryList: CategoryList[] = [
     {
       name: "Men's Fashion",
-      categoryImage: '../../../assets/categories/men2.jpg',
+      categoryImage:
+        'https://images.unsplash.com/photo-1441984904996-e0b6ba687e04?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       categoryLink: "men's-fashion",
     },
     {
       name: "Women's Fashion",
-      categoryImage: '../../../assets/categories/categories.jpg',
+      categoryImage:
+        'https://images.unsplash.com/photo-1532453288672-3a27e9be9efd?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       categoryLink: "women's-fashion",
     },
     {
       name: 'Baby & Toys',
-      categoryImage: '../../../assets/categories/baby.jpg',
+      categoryImage:
+        'https://images.unsplash.com/photo-1604467794349-0b74285de7e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       categoryLink: 'baby-and-toys',
     },
     {
       name: 'Beauty & Health',
-      categoryImage: '../../../assets/categories/beauty.jpg',
+      categoryImage:
+        'https://images.unsplash.com/photo-1540555700478-4be289fbecef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       categoryLink: 'beauty-and-health',
     },
     {
       name: 'Music',
-      categoryImage: '../../../assets/categories/music.jpg',
+      categoryImage:
+        'https://images.unsplash.com/photo-1511379938547-c1f69419868d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       categoryLink: 'music',
     },
     {
       name: 'Books',
-      categoryImage: '../../../assets/categories/books2.jpg',
+      categoryImage:
+        'https://images.unsplash.com/photo-1495446815901-a7297e633e8d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       categoryLink: 'books',
     },
   ];
